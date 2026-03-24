f558445cee4af3862ec4e6c3fa9fddf5af4dab00
1356050797 <41413393+1356050797@users.noreply.github.com>
2026-03-24 09:21:04 +0800

    Add 汤汤

--- data.js
+++ data.js
@@ -1,6 +1,22 @@
-// data.js
-const SITE_DATA = {
-    "one_map": [],      // 角色一图流
-    "char_material": [], // 角色养成素材
-    "weapon_data": []    // 武器养成与基质
-};
+const CHAR_CENTER = [
+    {
+        "name": "汤汤",
+        "mainImage": "https://bbs.hycdn.cn/image/2026/03/11/b3f147ff208fabfbfc775cbcef36bd16.png",
+        "one_map": {
+            "url": "https://bbs.hycdn.cn/image/common/20260312/110257/69b297f84298be0edf043199_3e93bcde.webp",
+            "author": "星语Poistars",
+            "link": "https://www.skland.com/profile?id=9593896117253"
+        },
+        "char_material": {
+            "url": "https://bbs.hycdn.cn/image/common/20260312/110257/69b28ab10b797bd73811b244_8c0325ea.webp",
+            "author": "星语Poistars",
+            "link": "https://www.skland.com/profile?id=9593896117253"
+        },
+        "weapon_data": {
+            "url": "",
+            "author": "",
+            "link": ""
+        },
+        "id": 1774315262436
+    }
+];
